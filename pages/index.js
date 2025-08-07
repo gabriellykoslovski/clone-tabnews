9f8039c69aea9c5e8effe1092e906904281772c5
gabrielly koslovski <gaby@DESKTOP-RQQS146.localdomain>
2025-08-07 16:10:14 -0300

    feat: adiciona scrips `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 function Home() {
-    return(
-        <main style={{height: "100vh", backgroundImage: "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdwallpapers.in%2Fdownload%2Flove_background-normal.jpg&f=1&nofb=1&ipt=add32d643c5d187054e2ba40c6dfee2b6018fe1958f9fe8f5c0b48e4c48650f4')"}}>
-            <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-                <h1 style={{color: 'white'}}>Parabéns!</h1>
-                <h3 style={{color: 'white'}}>Você é o melhor namorado do mundo!!</h3>
-                <img src="https://cdn3.emoji.gg/emojis/5639-bongo-cat-love.gif" />
-            </div>
-        </main>
-    )
+  return (
+    <main
+      style={{
+        height: "100vh",
+        backgroundImage:
+          "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdwallpapers.in%2Fdownload%2Flove_background-normal.jpg&f=1&nofb=1&ipt=add32d643c5d187054e2ba40c6dfee2b6018fe1958f9fe8f5c0b48e4c48650f4')",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <h1 style={{ color: "white" }}>Parabéns!</h1>
+        <h3 style={{ color: "white" }}>Você é o melhor namorado do mundo!!</h3>
+        <img src="https://cdn3.emoji.gg/emojis/5639-bongo-cat-love.gif" />
+      </div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
